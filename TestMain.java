00b5e1c23869dd9648c24b56fa78f618cb02dbf1
Alcides QS <alcidesqs@gmail.com>
2022-04-06 14:21:55 -0400

    Se añade comentarios en TestMain

--- TestMain.java
+++ TestMain.java
@@ -2,11 +2,12 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		
+		//Definicion de arreglo de numeros
 		int numbers[] = {20, 50, 15, 6, 80};
 		
-		System.out.println("----------------------------------------");
+		System.out.println("=======================================");
 		System.out.println("Ejemplo uso Patron Strategy (DIINF)");
-		System.out.println("----------------------------------------");
+		System.out.println("=======================================");
 
 		SortingContext context = new SortingContext();
 		
